262bfa366c3ed0bc7639b72182b187df039ffd4e
Cleiton Barreto <cleiton__br@hotmail.com>
2022-07-05 10:44:00 -0300

    Altera resultado do IMC arrendodado

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ function calculaImc(altura, peso){
   let elementoResultadoImc = document.getElementById("resultadoIMC")
 
   if (resultadoImc < 19) {
-      elementoResultadoImc.textContent = "Está abaixo do peso " + resultadoImc;
+      elementoResultadoImc.textContent = "Está abaixo do peso: " + Math.round(resultadoImc);
   } else if (resultadoImc >= 19 && resultadoImc < 25) {
-      elementoResultadoImc.textContent = "Peso está normal " + resultadoImc;
+      elementoResultadoImc.textContent = "Peso está normal: " + Math.round(resultadoImc);
   } else if (resultadoImc >= 25 && resultadoImc < 29) {
-      elementoResultadoImc.textContent = "Está sobrepeso " + resultadoImc;
+      elementoResultadoImc.textContent = "Está sobrepeso: " + Math.round(resultadoImc);
 }
     else if (resultadoImc >= 29 && resultadoImc < 35) {
-      elementoResultadoImc.textContent = "Está com obesidade grau 1 " + resultadoImc;
+      elementoResultadoImc.textContent = "Está com obesidade grau 1: " + Math.round(resultadoImc);
 }
     else if (resultadoImc >= 35 && resultadoImc < 40) {
-      elementoResultadoImc.textContent = "Está com obesidade grau 2 " + resultadoImc;
+      elementoResultadoImc.textContent = "Está com obesidade grau 2: " + Math.round(resultadoImc);
 }
     else if (resultadoImc >= 40) {
-      elementoResultadoImc.textContent = "Está com obesidade grau 3 " + resultadoImc;
+      elementoResultadoImc.textContent = "Está com obesidade grau 3: " + Math.round(resultadoImc);
 }
 }
 
